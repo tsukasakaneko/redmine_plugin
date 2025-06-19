b9e09d16f38405e9236835b106f2f59810c6d89c
tsukasakaneko <66767776+tsukasakaneko@users.noreply.github.com>
2025-06-19 20:28:03 +0000

    devcontainer追加

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+cd $REDMINE_ROOT
+
+ln -s /workspaces/${PLUGIN_NAME} plugins/${PLUGIN_NAME}
+if [ -f plugins/${PLUGIN_NAME}/Gemfile_for_test ]
+then
+    cp plugins/${PLUGIN_NAME}/Gemfile_for_test plugins/${PLUGIN_NAME}/Gemfile 
+fi
+
+
+bundle install 
+bundle exec rake redmine:plugins:migrate
+bundle exec rake redmine:plugins:migrate RAILS_ENV=test
+
+initdb() {
+    bundle exec rake db:create
+    bundle exec rake db:migrate
+    bundle exec rake redmine:plugins:migrate
+
+    bundle exec rake db:drop RAILS_ENV=test
+    bundle exec rake db:create RAILS_ENV=test
+    bundle exec rake db:migrate RAILS_ENV=test
+    bundle exec rake redmine:plugins:migrate RAILS_ENV=test
+}
+
+initdb
+
+export DB=mysql2
+export DB_NAME=redmine
+export DB_USERNAME=root
+export DB_PASSWORD=root
+export DB_HOST=mysql
+export DB_PORT=3306
+
+initdb
+
+export DB=postgresql
+export DB_NAME=redmine
+export DB_USERNAME=postgres
+export DB_PASSWORD=postgres
+export DB_HOST=postgres
+export DB_PORT=5432
+
+initdb
